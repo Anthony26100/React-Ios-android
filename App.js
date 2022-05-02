3f1f6853c79eac8b901bfc02d07a0f0b152f8e45
Anthony <anthony.bellier@laposte.net>
2022-05-02 11:58:34 +0200

    react native

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import dummy from './assets/dummy-image-square.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={ {color:'red', fontSize:20} }>Ici mon texte de mon app</Text>
+      <TouchableOpacity
+      onPress={()=> {alert("Votre photo")}} >
+        <Text style={styles.button}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button:{
+    backgroundColor: "red",
+    color:"white",
+    padding:"5px",
+    borderRadius:"5px"
   },
 });
