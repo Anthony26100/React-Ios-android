04d7162119aa51f9279387708a53f8133ac1ca8b
Anthony <anthony.bellier@laposte.net>
2022-05-02 14:28:14 +0200

    Partage extension
    expo-sharing

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import dummy from './assets/dummy-image-square.jpg';
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 export default function App() {
+  const [image,setImage] = useState(null);
+
   const openImagePicker = async () =>{
     const permission = await ImagePicker.requestMediaLibraryPermissionsAsync()
       console.log(permission);
@@ -11,14 +14,31 @@ export default function App() {
         alert('Permission to access your library is required !')
         return;
       }
+
     const pickerResult = await ImagePicker.launchImageLibraryAsync(); //Recuperer le liens de l'image
-    console.log(pickerResult);
+      if(pickerResult.cancelled) {
+        return;
+      }
+      setImage(pickerResult.uri);
+    
+  }
+
+  if(image !==null){
+    return(
+      <View style={styles.container}>
+        <Image source={ {uri: image} } style={ {width:300, height:300} }/>
+        <Text style={ {color:'red', fontSize:20} }>Ici mon texte de mon app</Text>
+        <TouchableOpacity
+        onPress={openImagePicker} >
+          <Text style={styles.button}>Pick a photo</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+    </View>)
   }
 
   return (
     <View style={styles.container}>
       <Image source={ dummy } style={ {width:300, height:300} }/>
-      <Image source={ {uri: 'https://cdn.pixabay.com/photo/2022/01/22/16/54/book-6957870_960_720.jpg'} } style={ {width:300, height:300} }/>
       <Text style={ {color:'red', fontSize:20} }>Ici mon texte de mon app</Text>
       <TouchableOpacity
       onPress={openImagePicker} >
@@ -39,5 +59,8 @@ const styles = StyleSheet.create({
   button:{
     backgroundColor: 'red',
     color:'white',
+    padding: '1%',
+    borderRadius: '15%',
+    textAlign: 'center',
   },
 });
